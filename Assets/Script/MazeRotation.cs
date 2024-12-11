5a3e7f39790eef39e05ed2c0c3882c3b4c877d48
Fadhli Hibatul Haqqi <fadhlimorth5@gmail.com>
2024-12-11 17:37:22 +0700

    add speed variable

--- Assets/Script/MazeRotation.cs
+++ Assets/Script/MazeRotation.cs
@@ -7,6 +7,8 @@ public class MazeRotation : MonoBehaviour
     // Atribut SerializeField untuk memunculkan variable di inspector
     [SerializeField]
     private Transform mazeTransform; // Membuat variable mazeTransform untuk menyimpan komponen Transform dari maze
+    [SerializeField]
+    private float speed; // Membuat variable speed untuk menyimpan data kecepatan putaran maze
 
     void Start()
     {
@@ -19,13 +21,13 @@ public class MazeRotation : MonoBehaviour
         if (Input.GetKey(KeyCode.A))
         {
             // Memutar maze di sumbu z sebesar 50 derajat tiap detik ke arah kiri
-            mazeTransform.Rotate(0, 0, 50 * Time.deltaTime);
+            mazeTransform.Rotate(0, 0, speed * Time.deltaTime);
         }
         // Jika input 'D' ditekan
         if (Input.GetKey(KeyCode.D))
         {
             // Memutar maze di sumbu z sebesar 50 derajat tiap detik ke arah kanan
-            mazeTransform.Rotate(0, 0, -50 * Time.deltaTime);
+            mazeTransform.Rotate(0, 0, -speed * Time.deltaTime);
         }
     }
 }
