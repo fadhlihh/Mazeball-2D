fcf428184f9ce236dba6ac1e69ba764e2b8caa14
Fadhli Hibatul Haqqi <fadhlimorth5@gmail.com>
2024-12-11 19:35:19 +0700

    add user interface

---
+++ Assets/Script/WinMenu.cs
@@ -0,0 +1,19 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class WinMenu : MonoBehaviour
+{
+    public void Retry()
+    {
+        // Mengulang permainan
+        Debug.Log("Retry");
+    }
+
+    public void Exit()
+    {
+        // Keluar Permainan
+        Debug.Log("Exit");
+    }
+}
