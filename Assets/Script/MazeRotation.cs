904f6eb3127895a2195b9116713eade1f2f424ab
Fadhli Hibatul Haqqi <fadhlimorth5@gmail.com>
2024-12-11 17:18:42 +0700

    add script

---
+++ Assets/Script/MazeRotation.cs
@@ -0,0 +1,19 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class MazeRotation : MonoBehaviour
+{
+    // Start is called before the first frame update
+    void Start()
+    {
+        // Kode ini digunakan untuk menampilkan log hello di console
+        Debug.Log("Hello");
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+
+    }
+}
